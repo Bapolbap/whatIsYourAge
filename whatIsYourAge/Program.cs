cab60cbbefee873ffe7f70654ab53a3276ba84c9
Fru1ty <pablo.jofre34@gmail.com>
2020-09-01 12:37:05 +0200

    andra uppgift klar
    :)))))

--- whatIsYourAge/Program.cs
+++ whatIsYourAge/Program.cs
@@ -6,7 +6,30 @@ namespace whatIsYourAge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //"Hello World" uppgiften gjorde jag som en WPF, så jag skapar ett nytt repo för den här
+
+            var age = "0";
+            String name;
+            String alive = "";
+
+            Console.WriteLine("what is your age? write an integer");
+            age = Console.ReadLine();
+            Console.WriteLine("what is your name?");
+            name = Console.ReadLine();
+            while(alive != "Yes" && alive != "No")
+            {
+                Console.WriteLine("are you alive? (Yes/No)");
+                alive = Console.ReadLine();
+            }
+            Console.WriteLine("you are " + age + " years old");
+            Console.WriteLine("your name is " + name);
+            if(alive == "Yes")
+            {
+                Console.WriteLine("you live");
+            } else
+            {
+                Console.WriteLine("you're dead");
+            }
         }
     }
 }
